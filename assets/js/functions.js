762cc43b7adce4c1372b813f06824c49577647ed
Milos Knezevic <ushtipak@gmail.com>
2023-02-01 01:12:27 +0100

    Initial commit :)

---
+++ assets/js/functions.js
@@ -0,0 +1,27 @@
+window.onkeydown = function(e) {
+    return !(e.keyCode == 32);
+};
+
+/*
+  Handles a click on the song played progress bar.
+*/
+document.getElementById('song-played-progress').addEventListener('click', function( e ){
+  var offset = this.getBoundingClientRect();
+  var x = e.pageX - offset.left;
+
+  Amplitude.setSongPlayedPercentage( ( parseFloat( x ) / parseFloat( this.offsetWidth) ) * 100 );
+});
+
+document.querySelector('img[data-amplitude-song-info="cover_art_url"]').style.height = document.querySelector('img[data-amplitude-song-info="cover_art_url"]').offsetWidth + 'px';
+
+Amplitude.init({
+  "shuffle_on": true,
+  "bindings": {
+    37: 'prev',
+    39: 'next',
+    32: 'play_pause'
+  },
+  "songs": [
+  ]
+});
+
